b51a0602b225e54ae75ceeac2fb42db85cd30783
unkokaeru <william@fayers.com>
2024-10-18 11:24:09 +0100

    feat: add config

--- source/logbookgenerator/config/constants.py
+++ source/logbookgenerator/config/constants.py
@@ -35,3 +35,18 @@ class Constants:
 
     # Default values
     DEFAULT_LOG_SAVE_PATH: Path = Path("logbookgenerator_log.txt")
+
+    # Type hints
+    TASK_ANNOTATION = dict[str, str | dict[str, list[tuple[str, str]]]]
+    WEEK_ANNOTATION = dict[str, str | dict[Literal["lab", "extra"], dict[str, TASK_ANNOTATION]]]
+
+    # Formatting
+    DATE_FORMAT = "%Y-%m-%d"
+    ANSWER_KEYWORD = "ANSWER"
+    INLINE_COMMENT_START = "/*"
+    BLOCK_COMMENT_START = "/**"
+    BLOCK_COMMENT_MIDDLE = "*"
+    COMMENT_END = "*/"
+    ANSWER_ID_DELIMITERS = "()"
+    INLINE_ANSWER_COMMENT = f"{ANSWER_KEYWORD} {r'\(Task (\d+).(\d+)\): (.+)'}"
+    CODE_COMMENT_DELIMITER = "```"

be5c6e580e3f178a08b42d265df45e3613489824
unkokaeru <william@fayers.com>
2024-10-18 23:30:04 +0100

    feat: add comment processing

--- source/logbookgenerator/computation/code_processing.py
+++ source/logbookgenerator/computation/code_processing.py
@@ -1,11 +1,78 @@
 """code_processing.py: Contains the functions for processing the code files."""
 
+import re
+
+from ..config.constants import Constants
 from . import logger
+from .comment_extraction import (
+    extract_block_comment,
+    extract_comment_content,
+    extract_comment_id,
+)
 
 logger = logger.getChild(__name__)
 
 
-def process_code_comments(code_lines: list[str]) -> list[str]:
+def process_inline_comment(
+    comment_line: str,
+    code_lines: list[str],
+    task_comments: dict[str, list[tuple[str, str]]],
+) -> None:
+    """
+    Process an inline comment to extract answer comments and related code.
+
+    Parameters
+    ----------
+    comment_line : str
+        The comment line.
+    code_lines : list[str]
+        The code lines.
+    task_comments : dict[str, list[tuple[str, str]]]
+        A dictionary where keys are task identifiers and values are lists of tuples
+        containing comments and associated code.
+    """
+    match = re.match(
+        Constants.INLINE_ANSWER_COMMENT, comment_line.strip(f"{Constants.INLINE_COMMENT_START} ")
+    )
+
+    if match:
+        comment_id = f"task_{match.group(1)}_{match.group(2)}"
+        comment_content = match.group(3).strip()
+
+        task_comments.setdefault(comment_id, [])
+        task_comments[comment_id].append((comment_content, "\n".join(code_lines)))
+
+
+def process_block_comment(
+    comment_lines: list[str],
+    code_lines: list[str],
+    task_comments: dict[str, list[tuple[str, str]]],
+) -> None:
+    """
+    Process a block comment to extract answer comments and related code.
+
+    Parameters
+    ----------
+    comment_lines : list[str]
+        The comment lines.
+    code_lines : list[str]
+        The code lines.
+    task_comments : dict[str, list[tuple[str, str]]]
+        A dictionary where keys are task identifiers and values are lists of tuples
+        containing comments and associated code.
+    """
+    if not comment_lines:
+        return
+
+    if comment_lines[0].startswith(Constants.ANSWER_KEYWORD):
+        comment_id = extract_comment_id(comment_lines[0])
+        comment_content = extract_comment_content(comment_lines)
+
+        task_comments.setdefault(comment_id, [])
+        task_comments[comment_id].append((comment_content, "\n".join(code_lines)))
+
+
+def process_code_comments(code_lines: list[str]) -> dict[str, list[tuple[str, str]]] | str:
     """
     Process the C++ code to extract answer comments and related code. If the code
     contains no answer comments, the function returns the entire code as a string.
@@ -21,5 +88,25 @@ def process_code_comments(code_lines: list[str]) -> list[str]:
         A dictionary where keys are task identifiers and values are lists of tuples
         containing comments and associated code.
     """
-    return []
-    # TODO: Implement code comment processing
+    task_comments: dict[str, list[tuple[str, str]]] = {}
+    current_code_lines: list[str] = []
+    line_number = 0
+
+    while line_number < len(code_lines):
+        line = code_lines[line_number].strip()
+
+        if line.startswith(Constants.BLOCK_COMMENT_START):
+            line_number += 1  # Skip the block comment start line
+            current_comment_lines, line_number = extract_block_comment(code_lines, line_number)
+            process_block_comment(current_comment_lines, current_code_lines, task_comments)
+        elif line.startswith(Constants.INLINE_COMMENT_START):
+            process_inline_comment(line, current_code_lines, task_comments)
+        else:
+            current_code_lines.append(line)
+
+        line_number += 1
+
+    if task_comments:
+        return task_comments
+
+    return "\n".join(current_code_lines)
---
+++ source/logbookgenerator/computation/comment_extraction.py
@@ -0,0 +1,115 @@
+"""comment_extraction.py: Contains the functions for extracting comments from the code files."""
+
+from ..config.constants import Constants
+from . import logger
+
+logger = logger.getChild(__name__)
+
+
+def extract_comment_id(comment_line: str) -> str:
+    """
+    Extract the comment identifier from the comment line.
+
+    Parameters
+    ----------
+    comment_line : str
+        The comment line.
+
+    Returns
+    -------
+    str
+        The comment identifier.
+    """
+    comment_id = comment_line.strip(f"{Constants.ANSWER_KEYWORD}{Constants.ANSWER_ID_DELIMITERS}: ")
+    return comment_id.replace(" ", "_").replace(".", "_")
+
+
+def extract_comment_code(comment_lines: list[str], line_number: int) -> tuple[str, int]:
+    """
+    Extract the comment and code from the comment lines.
+
+    Parameters
+    ----------
+    comment_lines : list[str]
+        The comment lines.
+    line_number : int
+        The line number of the comment start.
+
+    Returns
+    -------
+    str
+        The comment content.
+    int
+        The line number of the last line of the comment.
+    """
+    comment_code = ""
+
+    while line_number < len(comment_lines):
+        comment_line = comment_lines[line_number].strip()
+        if comment_line.startswith(Constants.CODE_COMMENT_DELIMITER):
+            break
+
+        comment_code += f"{comment_line}\n"
+        line_number += 1
+
+    return comment_code, line_number
+
+
+def extract_comment_content(comment_lines: list[str]) -> str:
+    """
+    Extract the comment content from the comment lines.
+
+    Parameters
+    ----------
+    comment_lines : list[str]
+        The comment lines.
+
+    Returns
+    -------
+    str
+        The comment content.
+    """
+    comment_content = ""
+    line_number = 1
+
+    while line_number < len(comment_lines):
+        comment_line = comment_lines[line_number].strip(f"{Constants.BLOCK_COMMENT_MIDDLE}")
+        if comment_line.startswith(Constants.CODE_COMMENT_DELIMITER):
+            code_content, line_number = extract_comment_code(comment_lines, line_number)
+            comment_content += f"{code_content}\n\n"
+
+        comment_content += f"{comment_line} "
+
+    return comment_content.strip()
+
+
+def extract_block_comment(code_lines: list[str], line_number: int) -> tuple[list[str], int]:
+    """
+    Extract a block comment from the code lines.
+
+    Parameters
+    ----------
+    code_lines : list[str]
+        The code lines.
+    line_number : int
+        The line number of the block comment start.
+
+    Returns
+    -------
+    list[str]
+        The block comment lines.
+    int
+        The line number of the last line of the block comment.
+    """
+    block_comment_lines: list[str] = []
+
+    while line_number < len(code_lines):
+        line = code_lines[line_number].strip()
+
+        if line == Constants.COMMENT_END:
+            break
+
+        block_comment_lines.append(line.lstrip(Constants.BLOCK_COMMENT_MIDDLE))
+        line_number += 1
+
+    return block_comment_lines, line_number

9318bcb2880a18f68f91c2c6566068491f3bcb6c
unkokaeru <unkokaeru@users.noreply.github.com>
2024-10-22 20:07:55 +0000

    chore: bump version from 1.0.8 to 1.0.9

--- source/logbookgenerator/__init__.py
+++ source/logbookgenerator/__init__.py
@@ -1,3 +1,3 @@
 """Generates a logbook. Designed for the University of Lincoln's Scientific Computing module."""
 
-__version__ = "1.0.8"
+__version__ = "1.0.9"
--- source/logbookgenerator/logs/__init__.py
+++ source/logbookgenerator/logs/__init__.py
@@ -1,3 +1,3 @@
 """Logging package for the project."""
 
-__version__ = "1.0.8"
+__version__ = "1.0.9"
